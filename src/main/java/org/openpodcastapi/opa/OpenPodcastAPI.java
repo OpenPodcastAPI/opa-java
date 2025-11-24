49a52f41b36165fc3f28947aaf08f4c972be097e
Ciarán Ainsworth <cda@sporiff.dev>
2025-11-24 21:20:01 +0100

    Migrate from ObjectMapper to JsonMapper

--- src/main/java/org/openpodcastapi/opa/OpenPodcastAPI.java
+++ src/main/java/org/openpodcastapi/opa/OpenPodcastAPI.java
@@ -1,9 +1,7 @@
 package org.openpodcastapi.opa;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -12,10 +10,5 @@ public class OpenPodcastAPI {
 
     static void main(String[] args) {
         SpringApplication.run(OpenPodcastAPI.class, args);
-    }
-
-    @Bean
-    public ObjectMapper objectMapper() {
-        return new ObjectMapper();
     }
 }
