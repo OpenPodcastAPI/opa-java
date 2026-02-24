69a6e6e4d2971ed78812a2e8a51789f03f2cb7cc
Ciarán Ainsworth <cda@sporiff.dev>
2026-02-24 16:26:04 +0100

    chore: add missing javadoc

--- src/main/java/org/openpodcastapi/opa/pagination/Cursorable.java
+++ src/main/java/org/openpodcastapi/opa/pagination/Cursorable.java
@@ -5,6 +5,13 @@ import java.time.Instant;
 /// An interface for results that can be paginated using a pagination.
 /// Any cursorable entity must have methods for fetching the database ID and created timestamp.
 public interface Cursorable {
+    /**
+     * @return the entity ID
+     */
     Long getId();
+
+    /**
+     * @return the entity's `created_at` timestamp
+     */
     Instant getCreatedAt();
 }
